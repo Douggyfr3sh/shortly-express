fe7602062929803b39cbd049c56b066988358652
Douglas Lyford <Douggyfr3sh@gmail.com>
2017-06-16 00:12:50 -0400

    Rewrote models/user.js to handle encrpytion and login.

--- app/models/user.js
+++ app/models/user.js
@@ -7,9 +7,25 @@ var Promise = require('bluebird');
 var User = db.Model.extend({
   //tablename
   tableName: 'users',
-  //hasTimestamps: true
-});
+  initialize: function() {
+    this.on('creating', this.hashPassword);
+  },
 
-console.log(User);
+  comparePassword: function(attempt, cb) {
+    bcrypt.compare(attemptedPassword, this.get('password'), (err, isMatch) => {
+      cb(isMatch);
+    });
+  },
+
+  hashPassword: function() {
+    var hasher = Promise.promisify(bcrypt.hash);
+
+    return hasher(this.get('password'), null, null).bind(this)
+      .then( (hash) => {
+        this.set('password', hash);
+      });
+  }
+
+});
 
 module.exports = User;
